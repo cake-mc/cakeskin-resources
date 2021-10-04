b63ce1173797acd54eaa34e952a0f98573585896
Big_Cake <bigcake@cakemc.top>
2021-10-04 17:28:20 +0800

    Delete public directory

--- public/meta.js
+++
@@ -1,1 +0,0 @@
-!function(){"use strict";const e=document.querySelector("#blessing-globals");window.blessing=JSON.parse(e.textContent),window.addEventListener("load",(()=>{navigator.serviceWorker.register("/sw.js?v6")}))}();
